6b0d4b442fa901e38d49bbc9197d7150b9f0f015
nvh12 <154247185+nvh12@users.noreply.github.com>
2025-03-30 22:49:19 +0700

    feat: Authen middleware - Middleware for verifying users and roles (for protected routes).

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,14 +1,31 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-exports.verifyToken = (req, res, next) => {
+async function verifyUser(req, res, next) {
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json({ message: 'Access Denied' });
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
+        const id = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(id);
+        if (!user) return res.status(404).json({message: 'User not found'});
+        req.user = { id: user._id, email: user.email, role: user.role};
         next();
     } catch (error) {
         return res.status(403).json({ message: 'Invalid or Expired Token' });
     }
 };
+
+function verifyRole(...allowRoles) {
+    return (req, res, next) => {
+        if (!req.user || !allowRoles.includes(req.user.role)) {
+            return res.status(401).json({ message: 'Access denied'});
+        }
+        next();
+    }
+}
+
+module.exports = {
+    verifyUser,
+    verifyRole
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5100;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
